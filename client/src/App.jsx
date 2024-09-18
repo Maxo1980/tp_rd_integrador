41898c2629baf0adee0a94d6582558308d05af16
stevendev43 <herstevendev@gmail.com>
2024-09-18 08:56:47 -0300

    creando proyecto React

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container mx-auto bg-red-600 h-screen'>
+        <h1 className='text-5xl font-light pt-10'>privet world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
