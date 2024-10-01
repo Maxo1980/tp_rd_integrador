feb351ffb49d2736b2a1b07ea32a8fce00edf688
stevendev43 <herstevendev@gmail.com>
2024-10-01 17:34:14 -0300

    avance de la pagina reclamos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Inicio from './pages/inicio';
 import Institucional from './Institucional.jsx';
+import Reclamos from './Reclamos.jsx';
 import Contacto from './Contacto.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/Institucional" element={<Institucional />} />
+        <Route path="/Reclamos" element={<Reclamos />} />
         <Route path="/Contacto" element={<Contacto />} />
       </Routes>
       <Footer />
